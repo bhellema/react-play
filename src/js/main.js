d9cd7387e7d0b6d555dd284edb11ac9cd84c3607
Ben Helleman <bhellema@adobe.com>
2016-04-07 11:38:57 -0400

    Extend the sample to build a few components and add data to the mix.

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,67 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var CommentBox = React.createClass({
+    displayName: 'CommentBox',
+
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data}/>
+                <CommentForm/>
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                CommentForm
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children.toString()}
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-  <h1>My First Crack At This</h1>,
-  document.getElementById('example')
+    <CommentBox data={data}/>,
+    document.getElementById('content')
 );
